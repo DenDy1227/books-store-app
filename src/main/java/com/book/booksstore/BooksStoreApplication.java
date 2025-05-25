70536ad509288a48e03b755560286a0830d7dd92
denDy1224 <denchik12327@gmail.com>
2025-05-25 19:03:20 +0200

    add @SuppressWarnings(HideUtilityClassConstructor)

--- src/main/java/com/book/booksstore/BooksStoreApplication.java
+++ src/main/java/com/book/booksstore/BooksStoreApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * The type Books store application.
  */
+@SuppressWarnings("HideUtilityClassConstructor")
 @SpringBootApplication
 public class BooksStoreApplication {
 
