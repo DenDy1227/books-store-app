198ecd32cbcab430ad16b583d60ab4be48e3d0e2
denDy1224 <denchik12327@gmail.com>
2025-05-27 21:07:15 +0200

    delete java doc in the book service interface

--- src/main/java/com/book/booksstore/BooksStoreApplication.java
+++ src/main/java/com/book/booksstore/BooksStoreApplication.java
@@ -1,21 +1,11 @@
 package com.book.booksstore;
 
-import com.book.booksstore.service.BookService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BooksStoreApplication {
     public static void main(final String[] args) {
         SpringApplication.run(BooksStoreApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner run(BookService userService) {
-        return args -> {
-            userService.findAll();
-        };
     }
 }
