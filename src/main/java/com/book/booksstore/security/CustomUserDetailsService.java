590080c53d942abd8879d61be095bf1b73c50d44
denDy1224 <denchik12327@gmail.com>
2025-06-20 10:38:18 +0200

    hw-7-spring-boot-sequrity

--- src/main/java/com/book/booksstore/security/CustomUserDetailsService.java
+++ src/main/java/com/book/booksstore/security/CustomUserDetailsService.java
@@ -15,7 +15,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
-        return userRepository.existsByEmail(email)
+        return userRepository.findByEmail(email)
                 .orElseThrow(() ->
                         new UsernameNotFoundException(
                                 "No user found with email: " + email
